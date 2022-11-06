630dc1d7c30531e69fbe804da19a13dee74f3ad9
Pedro Esparza <owsei@yahoo.com>
2022-11-07 00:30:32 +0100

    modificaciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit,ViewChild} from '@angular/core';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'SpinnerTest';
+
+  mostarSpinner:boolean =true;
+  valorCarga:number=0;
+
+  constructor(){
+    
+  }
+
+  ngOnInit() {
+      
+
+
+  }
+
+  sumar(){
+    if (this.valorCarga<100)
+      this.valorCarga+=10;
+  }
+
+  restar(){
+    if (this.valorCarga > 0)
+      this.valorCarga-=10;
+  }
+
+  ocultar(){
+    this.mostarSpinner=!this.mostarSpinner;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import 'hammerjs';
+
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import {MatButtonModule} from '@angular/material/button';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    MatProgressSpinnerModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatProgressBarModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
